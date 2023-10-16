b2d2b3c2fd00eb44f87b87b698bd9873c1f5bffe
tanvir321110 <tanvirahmed321110@gmail.com>
2023-10-17 02:49:48 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// const allProps = document.querySelectorAll('props')
+// document.addEventListener('click',function(){
+//     allProps.style.display='none'
+// })
+
+
+
+document.querySelector('.app').addEventListener('click',function(){
+    const language = document.querySelector('.app-drop-down');
+    language.style.display = 'block'
+    console.log(language)
+    alert('clik')
+})
